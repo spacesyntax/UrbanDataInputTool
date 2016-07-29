cd138c895fe088ed2baea121c2661bcf81c90565
Morri1234 <a.acharya@spacesyntax.com>
2016-07-29 18:05:47 +0100

    29072016 ver2

--- UrbanDataInput/CreateNew_dialog_base.py
+++ UrbanDataInput/CreateNew_dialog_base.py
@@ -1,25 +1,11 @@
 # -*- coding: utf-8 -*-
-"""
-/***************************************************************************
- UrbanDataInputDockWidget
-                                 A QGIS plugin
- Urban Data Input Tool for QGIS
-                             -------------------
-        begin                : 2016-06-03
-        git sha              : $Format:%H$
-        copyright            : (C) 2016 by Abhimanyu Acharya/(C) 2016 by Space Syntax Limited’.
-        email                : a.acharya@spacesyntax.com
- ***************************************************************************/
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-"""
+# Form implementation generated from reading ui file 'CreateNew_dialog_base.ui'
+#
+# Created: Fri Jul 29 17:17:15 2016
+#      by: PyQt4 UI code generator 4.10.2
+#
+# WARNING! All changes made in this file will be lost!
 
 from PyQt4 import QtCore, QtGui
 
